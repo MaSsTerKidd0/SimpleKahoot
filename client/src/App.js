a61bc620bda295a509869beeb77217274c0ceb20
ImTalos <robot1ba@gmail.com>
2024-08-12 14:09:30 +0300

    create branch for handling game quiz

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./components/home";
-import Register from "./components/register";
-import Login from "./components/login";
 import Quiz from "./components/quiz";
-import Profile from "./components/profile";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
         <Route path="/quiz" element={<Quiz />} />
-        <Route path="/profile" element={<Profile />} />
       </Routes>
     </Router>
   );
