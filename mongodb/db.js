a81e1cb16b3033ed7b13446cea8da8828fc96937
talos <talos@talos.com>
2024-08-15 18:33:41 +0300

    adding export for database & models for server usage

--- mongodb/db.js
+++ mongodb/db.js
@@ -5,3 +5,14 @@ const {User,Score,Game,Answer,Question} = models;
 
 mongoose.connect('mongodb://localhost:27017/kahoot');
     
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+module.exports = {
+    db,
+    models,
+};
