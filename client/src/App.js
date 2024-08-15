7b4d8c0b3f92b288cbf670924e7eceae191bcb12
talos <talos@talos.com>
2024-08-15 11:07:42 +0300

    adding quiz list compoment

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Quiz from "./components/quiz/quiz";
+import QuizList from "./components/quizList/quizList";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/quiz" element={<Quiz />} />
+        <Route path="/quizzes" element={<QuizList/>} />
+
       </Routes>
     </Router>
   );
